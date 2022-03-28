f4786bbf54fef322524e5d52299fc8ceec84a1c5
john <john@jpatmore.plus.com>
2022-03-28 19:54:42 +0100

    Add some comments at the top of main.py

--- main.py
+++ main.py
@@ -8,8 +8,17 @@ load_dotenv("E:/Python/EnvironmentVariables/.env")
 token = os.getenv("pixela_token")
 username = os.getenv("pixela_username")
 base_url = "https://pixe.la"
-graph_id = "graph1"
+graph_id = "graph2"
+# graph_id = "test-graph"
+# graph_id = "graph1"  # MAIN GRAPH
+
 # The graph can be viewed here: https://pixe.la/v1/users/sadsack963/graphs/graph1
+# and https://pixe.la/v1/users/sadsack963/graphs/graph1.html
+
+# See GET - /v1/users/<username>/graphs to get a list of your graphs
+#     https://docs.pixe.la/entry/get-graph
+
+# The user profile can be viewed here: https://pixe.la/@sadsack963
 
 
 # https://docs.python.org/3.9/library/datetime.html#strftime-and-strptime-format-codes
@@ -56,6 +65,9 @@ def create_account():
 
 def create_graph():
     endpoint_url = f"/v1/users/{username}/graphs"
+    # Interesting fact about the double slash::
+    #   endpoint_url = f"/v1/users//{username}/graphs"
+    # seems to return a list of user graphs!!
 
     params = {
         "id": graph_id,
@@ -117,9 +129,9 @@ def delete_account():
     print(response.text)
 
 
-# create_account()
-# create_graph()
-# add_pixel()
+# create_account()  # {"message":"Success. Let's visit https://pixe.la/@sadsack963 , it is your profile page!","isSuccess":true}
+# create_graph()  # {"message":"Success.","isSuccess":true}
+add_pixel()
 # get_pixel()
 # update_pixel()
 # delete_pixel()
